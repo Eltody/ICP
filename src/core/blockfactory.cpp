9be374c3adea9a7be914e07fc899713a4da6786b
Eltody <tomas.zatko.ms@gmail.com>
2021-04-04 11:51:12 +0200

    Another case rewrited to if else sequence

--- src/core/blockfactory.cpp
+++ src/core/blockfactory.cpp
@@ -1,13 +1,8 @@
 /** VUT FIT ICP
- * Predmet: Seminár C++ 2020/2021
- *
- * Názov súboru: blockfactory.cpp
- * Popis súboru: alokácia a dealokácia blokov
- *
- * Dátum: 3.4.2021
- * Autori:
- *        Zaťko Tomáš  - xzatko02
- *        Martin Rakús - xrakus04
+ * @file blockfactory.cpp
+ * @brief Alokácia a dealokácia blokov
+ * @author Tomáš Zaťko  - xzatko02
+ * @author Martin Rakús - xrakus04
  */
 
 
@@ -19,134 +14,133 @@
 
 
 // alokácia blokov
-BlockBase *BlockFactory::AllocBlock(BlockType t){
-	BlockBase *b;
-	switch (t) {
-	case VECTOR_INPUT:
-		b = new VectorInput(g);
-		break;
-    case VECTOR3D_INPUT:
-        b = new Vector3DInput(g);
-        break;
-	case VECTOR_OUTPUT:
-		b = new VectorOutput(g);
-		break;
-    case VECTOR_DOT_OUTPUT:
-        b = new VectorDotOutput(g);
-        break;
-    case VECTOR3D_OUTPUT:
-        b = new Vector3DOutput(g);
-        break;
-	case MAT2_INPUT:
-		b = new MatrixInput(g);
-		break;
-	case MAT2_OUTPUT:
-		b = new MatrixOutput(g);
-		break;
-    case MAT_DETERMINANT_INPUT:
-        b = new MatrixDeterminantInput(g);
-        break;
-    case MAT_DETERMINANT_OUTPUT:
-        b = new MatrixDeterminantOutput(g);
-        break;
-    case MAT3_INPUT:
-        b = new Matrix3x3Input(g);
-        break;
-    case MAT3_OUTPUT:
-        b = new Matrix3x3Output(g);
-        break;
-    case MATH_INPUT:
-        b = new MathInput(g);
-        break;
-    case MATH_OUTPUT:
-        b = new MathOutput(g);
-        break;
-	case VECTOR_ADD:
-		b = new VectorAddBlock(g);
-		break;
-    case VECTOR_SUB:
-        b = new VectorSubBlock(g);
-        break;
-    case VECTOR_ADD3D:
-        b = new VectorAddBlock3D(g);
-        break;
-    case VECTOR_SUB3D:
-        b = new VectorSubBlock3D(g);
-        break;
-    case VECTOR_DOTPRODUCT:
-        b = new VectorDotProductBlock2D(g);
-        break;
-    case VECTOR_MUL_CONST:
-        b = new VectorMulConstBlock(g);
-        break;
-    case VECTOR_MUL_CONST3D:
-        b = new VectorMulConstBlock3D(g);
-        break;
-    case MAT_ADD:
-        b = new MatrixAddBlock(g);
-        break;
-    case MAT_MUL:
-        b = new MatrixMulBlock(g);
-        break;
-    case MAT3_ADD:
-        b = new Matrix3x3AddBlock(g);
-        break;
-    case MAT3_MUL:
-        b = new Matrix3x3MulBlock(g);
-        break;
-	case MAT_MUL_VEC:
-		b = new MatMulVecBlock(g);
-		break;
-    case MAT_SUB:
-        b = new MatrixSubBlock(g);
-        break;
-    case MAT3_SUB:
-        b = new Matrix3x3SubBlock(g);
-        break;
-    case MAT_DETERMINANT:
-        b = new MatrixDeterminantBlock(g);
-        break;
-    case MATH_ADD:
-        b = new MathAddBlock(g);
-        break;
-    case MATH_SUB:
-        b = new MathSubBlock(g);
-        break;
-    case MATH_MUL:
-        b = new MathMulBlock(g);
-        break;
-    case MATH_DIV:
-        b = new MathDivBlock(g);
-        break;
-    case MATH_SQRT:
-        b = new MathSqrtBlock(g);
-        break;
-    case MATH_ABS:
-        b = new MathAbsBlock(g);
-        break;
-    case MATH_GT:
-        b = new MathGTBlock(g);
-        break;
-    case MATH_LT:
-        b = new MathLTBlock(g);
-        break;
-    case MATH_SIN:
-        b = new MathSinBlock(g);
-        break;
-    case MATH_COS:
-        b = new MathCosBlock(g);
-        break;
-    case MATH_INC:
-        b = new MathIncBlock(g);
-        break;
-    case MATH_DEC:
-        b = new MathDecBlock(g);
-        break;
-	default:
-		return nullptr;
-	}
-	blocks.push_back(b);
-	return b;
+BlockBase *BlockFactory::AllocBlock(BlockType option){
+    BlockBase *block;
+    if(option == VECTOR_INPUT){
+        block = new VectorInput(g);
+    }
+    else if( option == VECTOR3D_INPUT){
+        block = new Vector3DInput(g);
+    }
+    else if( option == VECTOR_OUTPUT){
+        block = new VectorOutput(g);
+    }
+    else if( option == VECTOR_DOT_OUTPUT){
+        block = new VectorDotOutput(g);
+    }
+    else if( option == VECTOR3D_OUTPUT){
+        block = new Vector3DOutput(g);
+    }
+    else if( option == MAT2_INPUT){
+        block = new MatrixInput(g);
+    }
+    else if( option == MAT2_OUTPUT){
+        block = new MatrixOutput(g);
+    }
+    else if( option == MAT_DETERMINANT_INPUT){
+        block = new MatrixDeterminantInput(g);
+    }
+    else if( option == MAT_DETERMINANT_OUTPUT){
+        block = new MatrixDeterminantOutput(g);
+    }
+    else if( option == MAT3_INPUT){
+        block = new Matrix3x3Input(g);
+    }
+    else if( option == MAT3_OUTPUT){
+        block = new Matrix3x3Output(g);
+    }
+    else if( option == MATH_INPUT){
+        block = new MathInput(g);
+    }
+    else if( option == MATH_OUTPUT){
+        block = new MathOutput(g);
+    }
+    else if( option == VECTOR_ADD){
+        block = new VectorAddBlock(g);
+    }
+    else if( option == VECTOR_SUB){
+        block = new VectorSubBlock(g);
+    }
+    else if( option == VECTOR_ADD3D){
+        block = new VectorAddBlock3D(g);
+    }
+    else if( option == VECTOR_SUB3D){
+        block = new VectorSubBlock3D(g);
+    }
+    else if( option == VECTOR_DOTPRODUCT){
+        block = new VectorDotProductBlock2D(g);
+    }
+    else if( option == VECTOR_MUL_CONST){
+        block = new VectorMulConstBlock(g);
+    }
+    else if( option == VECTOR_MUL_CONST3D){
+        block = new VectorMulConstBlock3D(g);
+    }
+    else if( option == MAT_ADD){
+        block = new MatrixAddBlock(g);
+    }
+    else if( option == MAT_MUL){
+        block = new MatrixMulBlock(g);
+    }
+    else if( option == MAT3_ADD){
+        block = new Matrix3x3AddBlock(g);
+    }
+    else if( option == MAT3_MUL){
+        block = new Matrix3x3MulBlock(g);
+    }
+    else if( option == MAT_MUL_VEC){
+        block = new MatMulVecBlock(g);
+    }
+    else if( option == MAT_SUB){
+        block = new MatrixSubBlock(g);
+    }
+    else if( option == MAT3_SUB){
+        block = new Matrix3x3SubBlock(g);
+    }
+    else if( option == MAT_DETERMINANT){
+        block = new MatrixDeterminantBlock(g);
+    }
+    else if( option == MATH_ADD){
+        block = new MathAddBlock(g);
+    }
+    else if( option == MATH_SUB){
+        block = new MathSubBlock(g);
+    }
+    else if( option == MATH_MUL){
+        block = new MathMulBlock(g);
+    }
+    else if( option == MATH_DIV){
+        block = new MathDivBlock(g);
+    }
+    else if( option == MATH_SQRT){
+        block = new MathSqrtBlock(g);
+    }
+    else if( option == MATH_ABS){
+        block = new MathAbsBlock(g);
+    }
+    else if( option == MATH_GT){
+        block = new MathGTBlock(g);
+    }
+    else if( option == MATH_LT){
+        block = new MathLTBlock(g);
+    }
+    else if( option == MATH_SIN){
+        block = new MathSinBlock(g);
+    }
+    else if( option == MATH_COS){
+        block = new MathCosBlock(g);
+    }
+    else if( option == MATH_INC){
+        block = new MathIncBlock(g);
+    }
+    else if( option == MATH_DEC){
+        block = new MathDecBlock(g);
+    }
+    else{
+        return nullptr;
+    }
+    blocks.push_back(block);
+    return block;
 }
 
 
@@ -154,15 +148,15 @@ BlockFactory::BlockFactory(Graph &g) : g(g) { }
 
 
 // uvoľnenie bloku
-void BlockFactory::FreeBlock(BlockBase *b){
-	blocks.remove(b);
-	delete b;
+void BlockFactory::FreeBlock(BlockBase *block){
+    blocks.remove(block);
+    delete block;
 }
 
 
 // uvoľnenie všetkých blokov
 BlockFactory::~BlockFactory(){
-	for(BlockBase *b : blocks){
-		delete b;
+    for(BlockBase *block : blocks){
+        delete block;
 	}
 }
