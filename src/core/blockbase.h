0f6eeac406a046448f83a2157649e8291ea56117
Eltody <tomas.zatko.ms@gmail.com>
2021-04-23 14:30:53 +0200

    changed comments

--- src/core/blockbase.h
+++ src/core/blockbase.h
@@ -21,89 +21,89 @@ class Graph;
  */
 class BlockBase {
 private:
-	/**
+    /**
 	 * @brief Vector of input ports
 	 */
 	std::vector<InPort> inputs; // Should be const vector of non const elements, but this requires custom implementation of vector!
-	/**
+    /**
 	 * @brief Vector of output ports
 	 */
 	std::vector<OutPort> outputs; // Should be const vector of non const elements, but this requires custom implementation of vector!
 protected:
-	BlockBase(Graph &g, BlockType type, std::string name);
+    BlockBase(Graph &g, BlockType type, std::string name);
 	BlockBase(Graph &g, BlockType type, std::string name,
 			  std::initializer_list<InPort> inputs,
 			  std::initializer_list<OutPort> outputs);
 public:
-	/**
+    /**
 	 * @brief A reference to the parent Graph object.
 	 */
 	Graph &graph;
-	/**
+    /**
 	 * @brief Type of the block.
 	 * See BlockType.
 	 */
 	const BlockType type;
-	/**
+    /**
 	 * @brief Human readable name of a block type
 	 */
 	const std::string name;
-	/**
+    /**
 	 * @brief Gets unique ID of a block
 	 * @return ID of a block
 	 */
 	int getID() const;
-	/**
+    /**
 	 * @brief Gets unique ID of a port
 	 * @param port Input port
 	 * @return ID of a port
 	 */
 	virtual int getPortID(const InPort &port) const;
-	/**
+    /**
 	 * @brief Gets unique ID of a port
 	 * @param port Output port
 	 * @return ID of a port
 	 */
 	virtual int getPortID(const OutPort &port) const;
-	/**
+    /**
 	 * @brief Gets address of a port
 	 * @param port ID of an input port
 	 * @return Address of an input port
 	 */
 	virtual InPort & Input(std::size_t id);
-	/**
+    /**
 	 * @brief Returns number of inputs
 	 * @return Number of inputs
 	 */
 	virtual std::size_t InputCount();
-	/**
+    /**
 	 * @brief Gets address of a port
 	 * @param port ID of an output port
 	 * @return Address of an output port
 	 */
 	virtual OutPort & Output(std::size_t id);
-	/**
+    /**
 	 * @brief Returns number of outputs
 	 * @return Number of inputs
 	 */
 	virtual std::size_t OutputCount();
 
-	/**
+    /**
 	 * @brief Checks if all inputs are non-null values
 	 * @return True when all inputs are non-null, false otherwise
 	 */
 	bool HasAllValues();
-	/**
+    /**
 	 * @brief Checks that all inputs are connected with some blocks
 	 * @return True when all inputs are connected, false otherwise
 	 */
 	bool InputsAreConnected();
 	virtual bool Computable();
-	/**
+    /**
 	 * @brief Sets all output port's values to Null
 	 */
 	void Reset();
-	/**
+    /**
 	 * @brief Virtual method that is implemented by every block type
 	 */
 	virtual void Compute() = 0;
